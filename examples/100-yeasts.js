73245aae78f49be7f7a69b13e18bc5e05bfdda8c
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-01-06 17:03:21 +0100

    Improved example.

--- examples/100-yeasts.js
+++ examples/100-yeasts.js
@@ -1,3 +1,9 @@
 for (var i = 0; i < 10; ++i)
 	for (var j = 0; j < 10; ++j)
-		yeast({x:10*i,y:10*j});
+		new yeast({
+			x: 10 * i,
+			y: 10 * j,
+			gfp: 100 + 10 * i * j,
+			rfp: 500 - 10 * i * j,
+			yfp: 100 + rand() % 200
+		});
