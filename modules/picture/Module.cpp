51ca33da5bada943790917bd44a3689c7023632b
Jiří Fatka <ntsfka@gmail.com>
2015-05-04 00:09:32 +0200

    Added picture module configuration.

--- modules/picture/Module.cpp
+++ modules/picture/Module.cpp
@@ -3,7 +3,9 @@
 #include "Module.hpp"
 
 // Simulation
+#include "core/Log.hpp"
 #include "simulator/Simulation.hpp"
+#include "parser/Parser.hpp"
 
 // Module
 #include "formats.hpp"
@@ -15,14 +17,49 @@ namespace picture {
 
 /* ************************************************************************ */
 
+void Module::configure(const simulator::ConfigurationBase& config)
+{
+    // File name pattern
+    {
+        auto filename = config.getString("filename");
+        if (!filename.empty())
+            setFileName(std::move(filename));
+    }
+
+    // Each step
+    {
+        auto each = config.getInteger("each");
+        if (each > 0)
+            setEachStep(each);
+    }
+}
+
+/* ************************************************************************ */
+
 void Module::draw(render::Context& context, const simulator::Simulation& simulation)
 {
+    const auto stepNumber = simulation.getStepNumber();
+
     // Skip first image, because it's not rendered yet
-    if (simulation.getStepNumber() == 0)
+    if (stepNumber == 0)
         return;
 
+    // Skip steps
+    if (stepNumber % getEachStep() != 0)
+        return;
+
+    auto filename = getFileName();
+    auto pos = filename.find("$1");
+
+    // Replace placeholder
+    if (pos != std::string::npos)
+        filename.replace(pos, 2, std::to_string(stepNumber));
+
     // Store current image (front buffer)
-    save_image(context, "image_" + std::to_string(simulation.getStepNumber()) + ".png");
+    if (!save_image(context, filename))
+    {
+        Log::warning("Unable to write image as: ", filename);
+    }
 }
 
 /* ************************************************************************ */
--- modules/picture/Module.hpp
+++ modules/picture/Module.hpp
@@ -24,6 +24,72 @@ namespace picture {
 class Module : public simulator::Module
 {
 
+
+// Public Accessors
+public:
+
+
+    /**
+     * @brief Return pattern for generated file name.
+     *
+     * @return
+     */
+    const std::string& getFileName() const noexcept
+    {
+        return m_fileName;
+    }
+
+
+    /**
+     * @brief Get number of step when image should be saved.
+     *
+     * @return
+     */
+    unsigned int getEachStep() const noexcept
+    {
+        return m_eachStep;
+    }
+
+
+// Public Mutators
+public:
+
+
+    /**
+     * @brief Set file name pattern. Currently is supported only with
+     * placeholder $1 that is replaced by step number.
+     *
+     * @param fileName
+     */
+    void setFileName(std::string fileName) noexcept
+    {
+        m_fileName = std::move(fileName);
+    }
+
+
+    /**
+     * @brief Set number of step when image should be saved.
+     *
+     * @param eachStep
+     */
+    void setEachStep(unsigned int eachStep) noexcept
+    {
+        m_eachStep = eachStep;
+    }
+
+
+// Public Operations
+public:
+
+
+    /**
+     * @brief Configure module.
+     *
+     * @param config
+     */
+    void configure(const simulator::ConfigurationBase& config) override;
+
+
     /**
      * @brief Render module.
      *
@@ -31,6 +97,17 @@ class Module : public simulator::Module
      * @param simulation
      */
     void draw(render::Context& context, const simulator::Simulation& simulation) override;
+
+
+// Private Data Members
+private:
+
+
+    /// Pattern for file name.
+    std::string m_fileName = "image_%ul.png";
+
+    /// Save image each n-th step.
+    unsigned int m_eachStep = 1;
 
 };
 
