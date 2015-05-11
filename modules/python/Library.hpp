3234ab19c0b95107ce65fe45b11632214697e98c
Jiří Fatka <ntsfka@gmail.com>
2015-05-11 23:02:17 +0200

    New Python module that will work as wrapper for Python modules.

---
+++ modules/python/Library.hpp
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#if !ENABLE_RENDER
+#error Unable to build module without ENABLE_RENDER
+#endif
+
+/* ************************************************************************ */
+
+// Simulator
+#include "../../simulator/Library.hpp"
+
+/* ************************************************************************ */
+
+DECLARE_LIBRARY;
+
+/* ************************************************************************ */
---
+++ modules/python/Module.hpp
@@ -0,0 +1,61 @@
+
+#pragma once
+
+/* ************************************************************************ */
+
+// Simulator
+#include "simulator/Module.hpp"
+#include "render/Context.hpp"
+
+/* ************************************************************************ */
+
+namespace module {
+namespace python {
+
+/* ************************************************************************ */
+
+/**
+ * @brief Module for Python support.
+ */
+class Module : public simulator::Module
+{
+
+// Public Ctors & Dtors
+public:
+
+
+    /**
+     * @brief Constructor.
+     *
+     * @param simulation
+     */
+    explicit Module(simulator::Simulation& simulation);
+
+
+// Public Accessors
+public:
+
+
+
+// Public Mutators
+public:
+
+
+
+// Public Operations
+public:
+
+
+
+// Private Data Members
+private:
+
+
+};
+
+/* ************************************************************************ */
+
+}
+}
+
+/* ************************************************************************ */
