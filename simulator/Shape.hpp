58bd98728987758327c5049af60b741719568e38
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-05-13 15:02:23 +0200

    Generalized method for shape to grid mapping.

--- simulator/Shape.hpp
+++ simulator/Shape.hpp
@@ -78,6 +78,89 @@ struct Shape
 
 /* ************************************************************************ */
 
+/**
+ * @brief Map shape to grid.
+ *
+ * @tparam OutIt Output iterator type.
+ * @tparam T     Coordinate value type.
+ * @tparam StepT Step type.
+ *
+ * @param out    Output iterator. It's required to be valid for all incrementations.
+ * @param shape  Circle shape
+ * @param center Circle center in grid coordinates.
+ * @param max    Maximum coordinates.
+ * @param mim    Minimum coordinates (default is {0, 0}).
+ */
+template<typename OutIt, typename T, typename StepT>
+OutIt mapShapeToGrid(OutIt out, const Shape& shape, const Vector<StepT>& steps,
+                     const Vector<T>& center,
+                     const Vector<T>& max, const Vector<T>& min = {})
+{
+    switch (shape.type)
+    {
+    default:
+        break;
+
+    case ShapeType::Circle:
+        return mapShapeToGrid(out, shape.circle, steps, center, max, min);
+    }
+
+    return out;
+}
+
+/* ************************************************************************ */
+
+/**
+ * @brief Map circle shape to grid.
+ *
+ * @tparam OutIt Output iterator type.
+ * @tparam T     Coordinate value type.
+ * @tparam StepT Step type.
+ *
+ * @param out    Output iterator. It's required to be valid for all incrementations.
+ * @param shape  Circle shape
+ * @param center Circle center in grid coordinates.
+ * @param max    Maximum coordinates.
+ * @param mim    Minimum coordinates (default is {0, 0}).
+ */
+template<typename OutIt, typename T, typename StepT>
+OutIt mapShapeToGrid(OutIt out, const ShapeCircle& shape, const Vector<StepT>& steps,
+                     const Vector<T>& center,
+                     const Vector<T>& max, const Vector<T>& min = {})
+{
+    // Radius steps in grid
+    const Vector<T> radiusSteps = shape.radius / steps;
+    const Vector<T> shapeCenter = center + shape.center / steps;
+
+    // Foreach grid given by radius steps
+    for (T x = -radiusSteps.getX(); x < radiusSteps.getX(); ++x)
+    {
+        for (T y = -radiusSteps.getY(); y < radiusSteps.getY(); ++y)
+        {
+            // Calculate normalized length
+            const Vector<T> xy{x, y};
+            const auto len = (xy / radiusSteps).getLength();
+
+            if (len > 1.f)
+                continue;
+
+            // Calculate grid coordinates
+            auto coord = shapeCenter + xy;
+
+            // Check if coordinates are in range
+            if (coord < min || coord >= max)
+                continue;
+
+            // Insert coordinates into output iterator
+            *out++ = coord;
+        }
+    }
+
+    return out;
+}
+
+/* ************************************************************************ */
+
 }
 
 /* ************************************************************************ */
