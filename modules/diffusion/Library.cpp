eb98dddfff03f9be1d08ff667716b3d30010464a
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-05-19 17:36:59 +0200

    Add remove signal programs.

--- modules/diffusion/Library.cpp
+++ modules/diffusion/Library.cpp
@@ -23,7 +23,7 @@
 
 static void generate_signal(simulator::Simulation& simulation, simulator::Object& obj, units::Duration dt, int signal)
 {
-    constexpr float SOURCE_STRENGTH = 1.f;
+    constexpr float SOURCE_STRENGTH = 0.2f;
 
     auto diff = simulation.useModule<module::diffusion::Module>("diffusion");
     assert(diff);
@@ -61,6 +61,46 @@ static void generate_signal(simulator::Simulation& simulation, simulator::Object
 
 /* ************************************************************************ */
 
+static void remove_signal(simulator::Simulation& simulation, simulator::Object& obj, units::Duration dt, int signal)
+{
+    constexpr float SOURCE_STRENGTH = 0.2f;
+
+    auto diff = simulation.useModule<module::diffusion::Module>("diffusion");
+    assert(diff);
+    auto& grid = diff->getGrid();
+
+    const Vector<float> start = simulation.getWorldSize() * -0.5;
+    const auto step = simulation.getWorldSize() / grid.getSize();
+
+    // Get cell position
+    const auto pos = obj.getPosition() - start;
+
+    // Check if position is in range
+    if (!pos.inRange(Vector<float>{0}, simulation.getWorldSize()))
+        return;
+
+    // Get grid position
+    Vector<unsigned int> coord = pos / step;
+
+    // TODO: use vector + unique (faster?)
+    std::set<decltype(coord)> coords;
+    const auto& shapes = obj.getShapes();
+    auto coordIt = std::inserter(coords, coords.end());
+
+    for (const auto& shape : shapes)
+    {
+        coordIt = simulator::mapShapeToGrid(coordIt, shape, step, coord, grid.getSize());
+    }
+
+    for (const auto& c : coords)
+    {
+        // Add signal
+        grid[c][signal] = std::max(grid[c][signal] - SOURCE_STRENGTH * dt, 0.f);
+    }
+}
+
+/* ************************************************************************ */
+
 DEFINE_LIBRARY_INIT(simulation)
 {
     assert(simulation);
@@ -79,6 +119,22 @@ DEFINE_LIBRARY_INIT(simulation)
 
     simulation->addProgram("diffusion.gen4", [simulation](simulator::Object& obj, units::Duration dt) {
         generate_signal(*simulation, obj, dt, 3);
+    });
+
+    simulation->addProgram("diffusion.rem1", [simulation](simulator::Object& obj, units::Duration dt) {
+        remove_signal(*simulation, obj, dt, 0);
+    });
+
+    simulation->addProgram("diffusion.rem2", [simulation](simulator::Object& obj, units::Duration dt) {
+        remove_signal(*simulation, obj, dt, 1);
+    });
+
+    simulation->addProgram("diffusion.rem3", [simulation](simulator::Object& obj, units::Duration dt) {
+        remove_signal(*simulation, obj, dt, 2);
+    });
+
+    simulation->addProgram("diffusion.rem4", [simulation](simulator::Object& obj, units::Duration dt) {
+        remove_signal(*simulation, obj, dt, 3);
     });
 }
 
