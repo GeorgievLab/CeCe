43a1db5eb11647e64c746332725d4a871a2c4530
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-06-12 12:18:04 +0200

    Added configuration for cell generator.

--- modules/cell/Generator.cpp
+++ modules/cell/Generator.cpp
@@ -7,11 +7,9 @@
 
 // Simulator
 #include "core/TimeMeasurement.hpp"
+#include "parser/Parser.hpp"
 #include "simulator/Simulation.hpp"
-
-// Module
-#include "Cell.hpp"
-#include "Yeast.hpp"
+#include "simulator/Object.hpp"
 
 /* ************************************************************************ */
 
@@ -22,31 +20,45 @@ namespace cell {
 
 void Generator::update(units::Duration dt, simulator::Simulation& simulation)
 {
-    auto _ = measure_time("cell.generator", [&simulation](std::ostream& out, const std::string& name, Clock::duration dt) {
-        out << name << ";" << simulation.getStepNumber() << ";" << std::chrono::duration_cast<std::chrono::microseconds>(dt).count() << "\n";
-    });
+    auto _ = measure_time("cell.generator", simulator::TimeMeasurementIterationOutput(&simulation));
 
     const float half = simulation.getWorldSize().getHeight() / 2.f;
 
     std::random_device rd;
     std::default_random_engine eng(rd());
 
-    std::bernoulli_distribution d(3.f * dt);
+    std::bernoulli_distribution genDist(getProbability());
     std::uniform_real_distribution<float> volume_d(30.f, 43.f);
 
-    // If cell should be generated
-    if (d(eng))
+    // If object should be generated
+    if (genDist(eng))
     {
         std::uniform_real_distribution<float> dist(-half, half);
         float y = dist(eng);
 
-        // Create cell
-        auto* c = simulation.createObject<module::cell::Yeast>();
-        c->setVolume(units::um3(volume_d(eng)));
-        c->setVelocity({10, 0});
-        c->setPosition({-simulation.getWorldSize().getWidth() / 2.f + 0.1f, y});
-        c->setGfp(20);
+        // Create object
+        auto* object = simulation.buildObject(getObjectClass());
+        //c->setVolume(units::um3(volume_d(eng)));
+        object->setVelocity(getStartVelocity());
+        object->setPosition({-simulation.getWorldSize().getWidth() / 2.f + 0.1f, y});
     }
+}
+
+/* ************************************************************************ */
+
+void Generator::configure(const simulator::Configuration& config, simulator::Simulation& simulation)
+{
+    config.callString("object", [this](const String& value) {
+        setObjectClass(value);
+    });
+
+    config.callIfSetString("probability", [this](const String& value) {
+        setProbability(parser::parse_value<units::Probability>(value));
+    });
+
+    config.callIfSetString("start-velocity", [this](const String& value) {
+        setStartVelocity(parser::parse_vector<VelocityVector::value_type>(value));
+    });
 }
 
 /* ************************************************************************ */
--- modules/cell/Generator.hpp
+++ modules/cell/Generator.hpp
@@ -5,6 +5,9 @@
 
 // Simulator
 #include "core/compatibility.hpp"
+#include "core/String.hpp"
+#include "core/Units.hpp"
+#include "core/VectorUnits.hpp"
 #include "simulator/Module.hpp"
 
 /* ************************************************************************ */
@@ -14,15 +17,84 @@ namespace cell {
 
 /* ************************************************************************ */
 
-class Module;
-
-/* ************************************************************************ */
-
 /**
  * @brief Cell generator module.
  */
 class Generator : public simulator::Module
 {
+
+// Public Accessors
+public:
+
+
+    /**
+     * @brief Returns generated object class.
+     *
+     * @return
+     */
+    const String& getObjectClass() const NOEXCEPT
+    {
+        return m_objectClass;
+    }
+
+
+    /**
+     * @brief Returns probability of geneating object.
+     *
+     * @return
+     */
+    units::Probability getProbability() const NOEXCEPT
+    {
+        return m_probability;
+    }
+
+
+    /**
+     * @brief Returns start velocity.
+     *
+     * @return
+     */
+    const VelocityVector& getStartVelocity() const NOEXCEPT
+    {
+        return m_startVelocity;
+    }
+
+
+// Public Mutators
+public:
+
+
+    /**
+     * @brief Set generated object class.
+     *
+     * @param objectClass
+     */
+    void setObjectClass(const String& objectClass) NOEXCEPT
+    {
+        m_objectClass = objectClass;
+    }
+
+
+    /**
+     * @brief Set probability of generating object.
+     *
+     * @param p
+     */
+    void setProbability(units::Probability p) NOEXCEPT
+    {
+        m_probability = p;
+    }
+
+
+    /**
+     * @brief Set object start velocity.
+     *
+     * @param velocity
+     */
+    void setStartVelocity(VelocityVector velocity)
+    {
+        m_startVelocity = std::move(velocity);
+    }
 
 
 // Public Operations
@@ -37,6 +109,26 @@ public:
      */
     void update(units::Duration dt, simulator::Simulation& simulation) override;
 
+
+    /**
+     * @brief Configure module.
+     *
+     * @param config
+     */
+    void configure(const simulator::Configuration& config, simulator::Simulation& simulation) override;
+
+
+// Private Data Members
+private:
+
+    /// Generated object class.
+    String m_objectClass;
+
+    /// Probability of generating object.
+    units::Probability m_probability = 0.5f;
+
+    /// Initial object velocity.
+    VelocityVector m_startVelocity = {10.f, 0.f};
 
 };
 
