fe00b5a51b5f5d03f84a71ad52f75c2f52669290
Vaclav Pelisek <peldax@gmail.com>
2015-07-03 23:54:56 +0200

    Expression Parser

--- core/ExpressionParser.cpp
+++ core/ExpressionParser.cpp
@@ -4,9 +4,147 @@
 
 class ExpressionParser
 {
+    
+private:
+
     Map<String, float> parameters;
     IteratorRange<const char*> iterator;
     
+    float signum(float source)
+    {
+        return (0 < source) - (source < 0);
+    }
+    
+    float add()
+    {
+        float value = multiply();
+        while (true)
+        {
+            if (*current == '+')
+            {
+                ++current;
+                value = value + multiply();
+            }
+            else if(*current == '-')
+            {
+                ++current;
+                value = value - multiply();
+            }
+            else
+                break;
+        }
+        return value;
+    }
+        
+    float multiply()
+    {
+        float value = power();
+        while (true)
+        {
+            if (*current == '*')
+            {
+                ++current;
+                value = value * power();
+            }
+            else if(*current == '/')
+            {
+                ++current;
+                value = value * power();
+            }
+            else
+                break;
+        }
+        return value;
+    }
+
+    float power()
+    {
+        float value = parenthesis();
+        while (true)
+        {
+            if (*current == '^')
+            {
+                ++current;
+                float exp = parenthesis();
+                if (value == 0 && exp == 0)
+                    return NAN;
+                else
+                    value = pow(value, exp);
+            }
+            else
+                break;
+        }
+        return value;
+    }
+
+    float parenthesis()
+    {
+        if (*current == '(')
+        {
+            ++current;
+            float value = add();
+            if (*current == ')')
+            {
+                ++current;
+                return value;
+            }
+            else
+                throw std::runtime_error("Parenthesis error.");
+        }
+        else
+            return function();
+    }
+
+    float function()
+    {
+        const char* end;
+        float value = strtof(iterator, &end);
+        if (current != end)
+        {
+            current = end;
+            return value;
+        }
+        std::string local = "";
+        while (std::find(operators.begin(), operators.end(), *iterator) == operators.end() && iterator != iterator.end());
+        {
+            ++iterator;
+            local += *iterator;
+        }
+        if (local == "Sin" || local == "sin")
+            return std::sin(parenthesis());
+        else if (local == "Cos" || local == "cos")
+            return std::cos(parenthesis());
+        else if (local == "Tan" || local == "tan")
+            return std::tan(parenthesis());
+        else if (local == "Sinh" || local == "sinh")
+            return std::sinh(parenthesis());
+        else if (local == "Cosh" || local == "cosh")
+            return std::cosh(parenthesis());
+        else if (local == "Tanh" || local == "tanh")
+            return std::tanh(parenthesis());
+        else if (local == "Sqrt" || local == "sqrt")
+            return std::sqrt(parenthesis());
+        else if (local == "Log" || local == "log")
+            return std::log10(parenthesis());
+        else if (local == "Ln" || local == "ln")
+            return std::log(parenthesis());
+        else if (local == "Sgn" || local == "sgn")
+            return signum(parenthesis());
+        else if (local == "Abs" || local == "abs")
+            return std::abs(parenthesis());
+        else
+        {
+            for (auto const &it : parameters)
+            {
+                if (local == it.first)
+                    return it.second;
+            }
+        }
+        throw std::runtime_error("Invalid expression.");
+    }
+    
+public:
+
     ExpressionParser(Map<String, float>& param, InteratorRange<const char*> range) NOEXCEPT
     : parameters(param), iterator(range)
     {
@@ -15,7 +153,7 @@ class ExpressionParser
     
     float parse()
     {
-        return 0;
+        return add();
     }
 }
 
