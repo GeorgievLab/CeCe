80b1f32d0e18e3cf7731160bfca012814537753d
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-07-08 16:37:28 +0200

    Rewritten diffusion module API. Added signal degradation. Use of real units of diffusion and degradation rate. Make signals separate (not computed in one type).

--- plugins/diffusion/Library.cpp
+++ plugins/diffusion/Library.cpp
@@ -1,13 +1,18 @@
-
+/* ************************************************************************ */
+/* Georgiev Lab (c) 2015                                                    */
+/* ************************************************************************ */
+/* Department of Cybernetics                                                */
+/* Faculty of Applied Sciences                                              */
+/* University of West Bohemia in Pilsen                                     */
 /* ************************************************************************ */
 
 // C++
 #include <cassert>
 #include <cstring>
-#include <set>
 
 // Simulator
 #include "core/Units.hpp"
+#include "core/DynamicArray.hpp"
 #include "simulator/Simulation.hpp"
 #include "simulator/ShapeToGrid.hpp"
 #include "simulator/Plugin.hpp"
@@ -25,14 +30,15 @@ using namespace simulator;
 
 static void generate_signal(Simulation& simulation, Object& obj, units::Duration dt, int signal)
 {
-    CONSTEXPR float SOURCE_STRENGTH = 0.2f;
+    //CONSTEXPR float SOURCE_STRENGTH = 0.2f;
+    CONSTEXPR float SOURCE_STRENGTH = 200.f;
 
     auto diff = simulation.useModule<plugin::diffusion::Module>("diffusion");
     assert(diff);
-    auto& grid = diff->getGrid();
 
+    const auto size = diff->getGridSize();
     const auto start = simulation.getWorldSize() * -0.5f;
-    const auto step = simulation.getWorldSize() / grid.getSize();
+    const auto step = simulation.getWorldSize() / size;
 
     // Get cell position
     const auto pos = obj.getPosition() - start;
@@ -44,27 +50,22 @@ static void generate_signal(Simulation& simulation, Object& obj, units::Duration
     // Get grid position
     auto coord = Vector<unsigned int>(pos / step);
 
-    // TODO: use vector + unique (faster?)
-    std::vector<decltype(coord)> coords;
+    DynamicArray<decltype(coord)> coords;
     const auto& shapes = obj.getShapes();
     auto coordIt = std::inserter(coords, coords.end());
 
     for (const auto& shape : shapes)
     {
-        coordIt = mapShapeToGrid(coordIt, shape, step, coord, grid.getSize());
+        coordIt = mapShapeToGrid(coordIt, shape, step, coord, size);
     }
 
-    std::sort(coords.begin(), coords.end(), [](const decltype(coord)& lhs, const decltype(coord)& rhs) {
-        return lhs < rhs;
-    });
-    coords.erase(std::unique(coords.begin(), coords.end(), [](const decltype(coord)& lhs, const decltype(coord)& rhs) {
-        return lhs == rhs;
-    }), coords.end());
+    std::sort(coords.begin(), coords.end());
+    coords.erase(std::unique(coords.begin(), coords.end()), coords.end());
 
     for (const auto& c : coords)
     {
         // Add signal
-        grid[c][signal] += SOURCE_STRENGTH * dt.value();
+        diff->getSignal(signal, c) += SOURCE_STRENGTH * dt.value();
     }
 }
 
@@ -76,10 +77,10 @@ static void remove_signal(Simulation& simulation, Object& obj, units::Duration d
 
     auto diff = simulation.useModule<plugin::diffusion::Module>("diffusion");
     assert(diff);
-    auto& grid = diff->getGrid();
 
+    const auto size = diff->getGridSize();
     const auto start = simulation.getWorldSize() * -0.5f;
-    const auto step = simulation.getWorldSize() / grid.getSize();
+    const auto step = simulation.getWorldSize() / size;
 
     // Get cell position
     const auto pos = obj.getPosition() - start;
@@ -92,26 +93,22 @@ static void remove_signal(Simulation& simulation, Object& obj, units::Duration d
     auto coord = Vector<unsigned int>(pos / step);
 
     // TODO: use vector + unique (faster?)
-    std::vector<decltype(coord)> coords;
+    DynamicArray<decltype(coord)> coords;
     const auto& shapes = obj.getShapes();
     auto coordIt = std::inserter(coords, coords.end());
 
     for (const auto& shape : shapes)
     {
-        coordIt = mapShapeToGrid(coordIt, shape, step, coord, grid.getSize());
+        coordIt = mapShapeToGrid(coordIt, shape, step, coord, size);
     }
 
-    std::sort(coords.begin(), coords.end(), [](const decltype(coord)& lhs, const decltype(coord)& rhs) {
-        return lhs < rhs;
-    });
-    coords.erase(std::unique(coords.begin(), coords.end(), [](const decltype(coord)& lhs, const decltype(coord)& rhs) {
-        return lhs == rhs;
-    }), coords.end());
+    std::sort(coords.begin(), coords.end());
+    coords.erase(std::unique(coords.begin(), coords.end()), coords.end());
 
     for (const auto& c : coords)
     {
         // Add signal
-        grid[c][signal] = std::max(grid[c][signal] - SOURCE_STRENGTH * dt.value(), 0.f);
+        diff->setSignal(signal, c, std::max(diff->getSignal(signal, c) - SOURCE_STRENGTH * dt.value(), 0.f));
     }
 }
 
--- plugins/diffusion/Module.hpp
+++ plugins/diffusion/Module.hpp
@@ -1,11 +1,24 @@
+/* ************************************************************************ */
+/* Georgiev Lab (c) 2015                                                    */
+/* ************************************************************************ */
+/* Department of Cybernetics                                                */
+/* Faculty of Applied Sciences                                              */
+/* University of West Bohemia in Pilsen                                     */
+/* ************************************************************************ */
 
 #pragma once
 
 /* ************************************************************************ */
 
+// C++
+#include <algorithm>
+
 // Simulator
 #include "core/compatibility.hpp"
+#include "core/String.hpp"
+#include "core/Units.hpp"
 #include "core/Grid.hpp"
+#include "core/Range.hpp"
 #include "simulator/Module.hpp"
 
 #if ENABLE_RENDER
@@ -13,10 +26,6 @@
 #include "render/Object.hpp"
 #include "render/GridColorSmooth.hpp"
 #endif
-
-// Module
-#include "Signal.hpp"
-#include "SignalGrid.hpp"
 
 /* ************************************************************************ */
 
@@ -35,8 +44,41 @@ class Module : public simulator::Module
 public:
 
 
-    /// Coefficients type.
-    using Coefficients = Signal;
+    /// Signal value type.
+    using Signal = float;
+
+    /// Signal identificator.
+    using SignalId = unsigned int;
+
+    /// Grid type.
+    using GridType = Grid<Signal>;
+
+    /// Grid type.
+    using Coordinate = typename GridType::CoordinateType;
+
+    /// Grid size type
+    using SizeType = Vector<typename GridType::SizeType>;
+
+    /// Diffusion rate (m^2/s).
+    using DiffusionRate = units::Unit<units::List<units::BaseLength, units::BaseLength>, units::List<units::BaseTime>>;
+
+    /// Degradation rate (%/s).
+    using DegradationRate = units::Unit<units::List<>, units::List<units::BaseTime>>;
+
+
+// Public Contants
+public:
+
+
+    /**
+     * This value is used to diffuse grid cell into OFFSET cell around.
+     * It also extend inner grid size by 2 * OFFSET so then the boundary
+     * checking is not need.
+     */
+    static constexpr typename Coordinate::value_type OFFSET = 1;
+
+    /// Invalid signal ID.
+    static constexpr SignalId INVALID_SIGNAL_ID = -1;
 
 
 // Public Ctors & Dtors
@@ -60,35 +102,282 @@ public:
 
 
     /**
-     * @brief Returns grid.
+     * @brief Returns signal grid size.
      *
-     * @return
+     * @return Current size.
      */
-    SignalGrid& getGrid() NOEXCEPT
+    const SizeType& getGridSize() const noexcept
     {
-        return m_grid;
+        return m_gridSize;
     }
 
 
     /**
-     * @brief Returns grid.
+     * @brief Returns number of signals.
      *
-     * @return
+     * @return Number of signals.
      */
-    const SignalGrid& getGrid() const NOEXCEPT
+    size_t getSignalCount() const noexcept
     {
-        return m_grid;
+        return m_gridsFront.size();
     }
 
 
     /**
-     * @brief Returns diffussion coefficients.
+     * @brief Returns range of signal IDs.
      *
      * @return
      */
-    Coefficients getCoefficients() const NOEXCEPT
+    IteratorRange<ValueIterator<SignalId>> getSignalIds() const noexcept
     {
-        return m_coefficients;
+        return range<SignalId>(0u, getSignalCount());
+    }
+
+
+    /**
+     * @brief Returns signal identifier.
+     *
+     * @param name Signal name.
+     *
+     * @return Signal identifier or INVALID_SIGNAL_ID
+     */
+    SignalId getSignalId(const String& name) const noexcept
+    {
+        auto it = std::find(m_names.begin(), m_names.end(), name);
+        return it != m_names.end() ? std::distance(m_names.begin(), it) : INVALID_SIGNAL_ID;
+    }
+
+
+    /**
+     * @brief Returns mutable signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    Signal& getSignalFront(SignalId id, const Coordinate& coord) noexcept
+    {
+        return m_gridsFront[id][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns immutable signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    const Signal& getSignalFront(SignalId id, const Coordinate& coord) const noexcept
+    {
+        return m_gridsFront[id][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns mutable signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    Signal& getSignalFront(const String& name, const Coordinate& coord) noexcept
+    {
+        return m_gridsFront[getSignalId(name)][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns immutable signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    const Signal& getSignalFront(const String& name, const Coordinate& coord) const noexcept
+    {
+        return m_gridsFront[getSignalId(name)][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns mutable signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    Signal& getSignalBack(SignalId id, const Coordinate& coord) noexcept
+    {
+        return m_gridsBack[id][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns immutable signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    const Signal& getSignalBack(SignalId id, const Coordinate& coord) const noexcept
+    {
+        return m_gridsBack[id][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns mutable signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    Signal& getSignalBack(const String& name, const Coordinate& coord) noexcept
+    {
+        return m_gridsBack[getSignalId(name)][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns immutable signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    const Signal& getSignalBack(const String& name, const Coordinate& coord) const noexcept
+    {
+        return m_gridsBack[getSignalId(name)][coord + OFFSET];
+    }
+
+
+    /**
+     * @brief Returns mutable signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    Signal& getSignal(SignalId id, const Coordinate& coord) noexcept
+    {
+        return getSignalFront(id, coord);
+    }
+
+
+    /**
+     * @brief Returns immutable signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    const Signal& getSignal(SignalId id, const Coordinate& coord) const noexcept
+    {
+        return getSignalFront(id, coord);
+    }
+
+
+    /**
+     * @brief Returns mutable signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    Signal& getSignal(const String& name, const Coordinate& coord) noexcept
+    {
+        return getSignalFront(name, coord);
+    }
+
+
+    /**
+     * @brief Returns immutable signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     *
+     * @return
+     */
+    const Signal& getSignal(const String& name, const Coordinate& coord) const noexcept
+    {
+        return getSignalFront(name, coord);
+    }
+
+
+    /**
+     * @brief Returns signal diffussion rate.
+     *
+     * @param id Signal identifier.
+     *
+     * @return
+     */
+    DiffusionRate getDiffusionRate(SignalId id) const noexcept
+    {
+        return m_diffusionRates[id];
+    }
+
+
+    /**
+     * @brief Returns signal diffussion rate.
+     *
+     * @param name Signal name.
+     *
+     * @return
+     */
+    DiffusionRate getDiffusionRate(const String& name) const noexcept
+    {
+        return m_diffusionRates[getSignalId(name)];
+    }
+
+
+    /**
+     * @brief Returns signal degradation rate.
+     *
+     * @param id Signal identifier.
+     *
+     * @return
+     */
+    DegradationRate getDegradationRate(SignalId id) const noexcept
+    {
+        return m_degradationRates[id];
+    }
+
+
+    /**
+     * @brief Returns signal degradation rate.
+     *
+     * @param name Signal name.
+     *
+     * @return
+     */
+    DegradationRate getDegradationRate(const String& name) const noexcept
+    {
+        return m_degradationRates[getSignalId(name)];
+    }
+
+
+    /**
+     * @brief Check if coordinates are in range.
+     *
+     * @param coord Tested coordinates.
+     *
+     * @return
+     */
+    bool inRange(const Coordinate& coord) const noexcept
+    {
+        return coord.inRange(Coordinate{0}, m_gridSize);
     }
 
 
@@ -97,37 +386,158 @@ public:
 
 
     /**
-     * @brief Resize signal grid.
+     * @brief Resize signal grids.
      *
      * @param size New grid size.
      */
-    void setSize(const Vector<SignalGrid::SizeType>& size)
+    void setGridSize(SizeType size);
+
+
+    /**
+     * @brief Register new signal.
+     *
+     * @param name    Signal name.
+     * @param rate    Diffusion rate.
+     * @param degRate Degradation rate.
+     */
+    void registerSignal(String name, DiffusionRate rate, DegradationRate degRate = {});
+
+
+    /**
+     * @brief Set signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     * @param value Signal value.
+     *
+     * @return
+     */
+    void setSignalFront(SignalId id, const Coordinate& coord, Signal value) noexcept
     {
-        m_grid.resize(size);
-        m_gridBack.resize(size);
+        m_gridsFront[id][coord + OFFSET] = value;
     }
 
 
     /**
-     * @brief Change diffussion coefficients.
+     * @brief Set signal value at given coordinates.
      *
-     * @param coefficients
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     * @param value Signal value.
+     *
+     * @return
      */
-    void setCoefficients(Coefficients coefficients) NOEXCEPT
+    void setSignalFront(const String& name, const Coordinate& coord, Signal value) noexcept
     {
-        m_coefficients = coefficients;
+        m_gridsFront[getSignalId(name)][coord + OFFSET] = value;
     }
 
 
     /**
-     * @brief Change diffussion coefficient.
+     * @brief Set signal value at given coordinates.
      *
-     * @param pos
-     * @param value
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     * @param value Signal value.
+     *
+     * @return
      */
-    void setCoefficient(unsigned int pos, float value) NOEXCEPT
+    void setSignalBack(SignalId id, const Coordinate& coord, Signal value) noexcept
     {
-        m_coefficients[pos] = value;
+        m_gridsBack[id][coord + OFFSET] = value;
+    }
+
+
+    /**
+     * @brief Set signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     * @param value Signal value.
+     *
+     * @return
+     */
+    void setSignalBack(const String& name, const Coordinate& coord, Signal value) noexcept
+    {
+        m_gridsBack[getSignalId(name)][coord + OFFSET] = value;
+    }
+
+
+    /**
+     * @brief Set signal value at given coordinates.
+     *
+     * @param id    Signal identifier.
+     * @param coord Coordinates.
+     * @param value Signal value.
+     *
+     * @return
+     */
+    void setSignal(SignalId id, const Coordinate& coord, Signal value) noexcept
+    {
+        setSignalFront(id, coord, value);
+    }
+
+
+    /**
+     * @brief Set signal value at given coordinates.
+     *
+     * @param name  Signal name.
+     * @param coord Coordinates.
+     * @param value Signal value.
+     *
+     * @return
+     */
+    void setSignal(const String& name, const Coordinate& coord, Signal value) noexcept
+    {
+        setSignalFront(name, coord, value);
+    }
+
+
+    /**
+     * @brief Set signal diffussion rate.
+     *
+     * @param id   Signal identifier.
+     * @param rate Diffusion rate.
+     */
+    void setDiffusionRate(SignalId id, DiffusionRate rate) noexcept
+    {
+        m_diffusionRates[id] = rate;
+    }
+
+
+    /**
+     * @brief Set signal diffussion rate.
+     *
+     * @param name Signal name.
+     * @param rate Diffusion rate.
+     */
+    void setDiffusionRate(const String& name, DiffusionRate rate) noexcept
+    {
+        m_diffusionRates[getSignalId(name)] = rate;
+    }
+
+
+    /**
+     * @brief Set signal degradation rate.
+     *
+     * @param id   Signal identifier.
+     * @param rate Degradation rate.
+     */
+    void setDegradationRate(SignalId id, DegradationRate rate) noexcept
+    {
+        m_degradationRates[id] = rate;
+    }
+
+
+    /**
+     * @brief Set signal degradation rate.
+     *
+     * @param name Signal name.
+     * @param rate Degradation rate.
+     */
+    void setDegradationRate(const String& name, DegradationRate rate) noexcept
+    {
+        m_degradationRates[getSignalId(name)] = rate;
     }
 
 
@@ -168,28 +578,84 @@ public:
     /**
      * @brief Update drawable.
      */
-    void updateDrawable();
+    void updateDrawable() const;
 #endif
+
+
+    /**
+     * @brief Swap signal grids.
+     *
+     * @param id Signal identifier.
+     */
+    void swap(SignalId id) noexcept
+    {
+        std::swap(m_gridsFront[id], m_gridsBack[id]);
+    }
+
+
+    /**
+     * @brief Clear front grid.
+     *
+     * @param id Signal identifier.
+     */
+    void clearFront(SignalId id) noexcept
+    {
+        std::fill(m_gridsFront[id].begin(), m_gridsFront[id].end(), Signal{});
+    }
+
+
+    /**
+     * @brief Clear back grid.
+     *
+     * @param id Signal identifier.
+     */
+    void clearBack(SignalId id) noexcept
+    {
+        std::fill(m_gridsBack[id].begin(), m_gridsBack[id].end(), Signal{});
+    }
+
+
+// Protected Operations
+protected:
+
+
+    /**
+     * @brief Update signal.
+     *
+     * @param step Size of one grid cell.
+     * @param dt   Delta time.
+     * @param id   Signal identifier.
+     */
+    void updateSignal(const PositionVector& step, units::Time dt, SignalId id);
 
 
 // Private Data Members
 private:
 
 
-    /// Diffusion coeffients.
-    Coefficients m_coefficients{1.f};
+    // Grid size without borders.
+    SizeType m_gridSize;
 
-    /// Current signal grid.
-    SignalGrid m_grid;
+    /// Signal names.
+    DynamicArray<String> m_names;
 
-    /// Signal grid used for updating.
-    SignalGrid m_gridBack;
+    /// Diffusion rates.
+    DynamicArray<DiffusionRate> m_diffusionRates;
+
+    /// Degradation rates.
+    DynamicArray<DegradationRate> m_degradationRates;
+
+    /// Front signal grids.
+    DynamicArray<GridType> m_gridsFront;
+
+    /// Back signal grids.
+    DynamicArray<GridType> m_gridsBack;
+
+    /// Signal colors.
+    DynamicArray<render::Color> m_colors;
 
     /// Background color.
     render::Color m_background = render::colors::BLACK;
-
-    /// Signal colors.
-    std::vector<render::Color> m_colors;
 
 #if ENABLE_RENDER
     /// Drawable signal grid.
