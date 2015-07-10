0940b815d8bc0de594b27c58fbbafd044348bac4
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-07-10 16:17:03 +0200

    New plugin stochastic-reactions-diffusive for diffusive stochastic reactions.

---
+++ plugins/stochastic-reactions-diffusive/Library.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************ */
+/* Georgiev Lab (c) 2015                                                    */
+/* ************************************************************************ */
+/* Department of Cybernetics                                                */
+/* Faculty of Applied Sciences                                              */
+/* University of West Bohemia in Pilsen                                     */
+/* ************************************************************************ */
+/* Author: Jiří Fatka <fatkaj@ntis.zcu.cz>                                  */
+/* Author: Václav Pelíšek <pelisekv@students.zcu.cz>                        */
+/* ************************************************************************ */
+
+// Simulator
+#include "simulator/Plugin.hpp"
+#include "simulator/PluginApi.hpp"
+
+/************************************************************************** */
+
+using namespace simulator;
+
+/* ************************************************************************ */
+
+class StochasticReactionsDiffusiveApi : public PluginApi
+{
+    Program createProgram(Simulation& simulation, const String& name, String code = {}) override
+    {
+        return {};
+    }
+};
+
+/* ************************************************************************ */
+
+DEFINE_PLUGIN(stochastic_reactions_diffusive, StochasticReactionsDiffusiveApi)
+
+/* ************************************************************************ */
---
+++ plugins/stochastic-reactions-diffusive/test/test.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************ */
+/* Department of Cybernetics                                                */
+/* Faculty of Applied Sciences                                              */
+/* University of West Bohemia in Pilsen                                     */
+/* ************************************************************************ */
+
+// GTest
+#include <gtest/gtest.h>
+
+/* ************************************************************************ */
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+/* ************************************************************************ */
