e6003e2cddcd70c1376a905c384c82caedec008e
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-07-12 11:20:43 +0200

    Improved MacOS bundle fixup script.

--- platform/MacOS/bundle.sh
+++ platform/MacOS/bundle.sh
@@ -1,24 +1,73 @@
 #!/bin/bash
 
-BINARY=$1
+# ######################################################################### #
+# Georgiev Lab (c) 2015                                                     #
+# ######################################################################### #
+# Department of Cybernetics                                                 #
+# Faculty of Applied Sciences                                               #
+# University of West Bohemia in Pilsen                                      #
+# ######################################################################### #
+# Author: Jiří Fatka <fatkaj@ntis.zcu.cz>                                   #
+# ######################################################################### #
 
+FRAMEWORKS=../Frameworks
+SOURCE_DIR=/usr/local
+
+# Directory of the binary
+DIRECTORY=`dirname $BINARY`
+
+# Get dependency of given library
+function get_dependency()
+{
+    otool -L $1 | grep -oE "([^ ]*).dylib"
+}
+
+# Fix binary libraries
+function fix_libraries()
+{
+    BINARY=$1
+
+    for LIBRARY in `get_dependency $BINARY`
+    do
+        fix_library $BINARY $LIBRARY
+    done
+}
+
+# Fix library
 function fix_library()
 {
-    LIBRARY=$1
+    BINARY=$1
+    LIBRARY=$2
 
     if [[ $LIBRARY == *"/"* ]]
     then
-        #install_name_tool -change $BINARY @executable_path/../Frameworks/
-        echo "Pass"
+        BASENAME=`basename ${LIBRARY}`
+        BUNDLE_PATH="${DIRECTORY}/${FRAMEWORKS}"
+        PATH="${BUNDLE_PATH}/${BASENAME}"
+
+        # Only libraries from sources
+        if [[ $LIBRARY == "${SOURCE_DIR}"* ]]
+        then
+            if [[ ! -f "${PATH}" ]]
+            then
+                # Copy library into bundle
+                cp "${LIBRARY}" "${BUNDLE_PATH}"
+
+                # Recursive fix
+                fix_libraries $PATH
+            fi
+
+            echo "Update '${BINARY}' for '${LIBRARY}'"
+            install_name_tool -change ${LIBRARY} @executable_path/${FRAMEWORKS}/${BASENAME} ${BINARY}
+        fi
     else
         # Local
-        install_name_tool -change $LIBRARY @executable_path/../Frameworks/$LIBRARY $BINARY
+        echo "Update '${BINARY}' for '${LIBRARY}'"
+        install_name_tool -change ${LIBRARY} @executable_path/${FRAMEWORKS}/${LIBRARY} ${BINARY}
     fi
 }
 
-LIBRARIES=$(otool -L $BINARY | grep -oE "([^ ]*).dylib")
+# Source binary
+fix_libraries $1
 
-for LIBRARY in $LIBRARIES
-do
-    fix_library $LIBRARY
-done
+# ######################################################################### #
