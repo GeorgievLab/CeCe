e8250a271a84f855beb8bc3c953a7443bef0af23
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-07-13 13:02:25 +0200

    Create new files Diffusion.hpp/cpp (and removed old one) that contains to function for communicate with diffusion in required way.

--- plugins/stochastic-reactions-diffusive/Diffusion.cpp
+++ plugins/stochastic-reactions-diffusive/Diffusion.cpp
@@ -1,18 +1,26 @@
+/* ************************************************************************ */
+/* Georgiev Lab (c) 2015                                                    */
+/* ************************************************************************ */
+/* Department of Cybernetics                                                */
+/* Faculty of Applied Sciences                                              */
+/* University of West Bohemia in Pilsen                                     */
+/* ************************************************************************ */
+/* Author: Jiří Fatka <fatkaj@ntis.zcu.cz>                                  */
+/* ************************************************************************ */
 
 // Declaration
-#include "ReactionDiffusive.hpp"
+#include "Diffusion.hpp"
 
 // C++
 #include <random>
 
 // Simulator
-#include "core/String.hpp"
-#include "core/Vector.hpp"
 #include "simulator/ShapeToGrid.hpp"
 
-// Plugins
-#include "plugins/cell/CellBase.hpp"
-#include "plugins/diffusion/Module.hpp"
+/* ************************************************************************ */
+
+namespace plugin {
+namespace stochastic_reactions {
 
 /* ************************************************************************ */
 
@@ -26,13 +34,10 @@ std::mt19937 g_randomEngine(g_randomDevice());
 
 /* ************************************************************************ */
 
-/**
- * @brief Returns coordinates in diffusion grid for given cell object.
- *
- * @param gridSize  Size of diffusion grid.
- * @param worldSize Simulation world size.
- * @param cell      Cell object.
- */
+}
+
+/* ************************************************************************ */
+
 DynamicArray<plugin::diffusion::Module::Coordinate> getCoordinates(
     const plugin::diffusion::Module::SizeType& gridSize,
     const SizeVector& worldSize,
@@ -69,18 +74,9 @@ DynamicArray<plugin::diffusion::Module::Coordinate> getCoordinates(
 
 /* ************************************************************************ */
 
-/**
- * @brief Increase amount of molecules outside of cell.
- *
- * @param diffusion Diffusion module. Obtained by <code>simulation.useModule<plugin::diffusion::Module>("diffusion");</code>
- * @param worldSize Simulation world size. <code>simulation.getWorldSize();</code>
- * @param cell      Cell object.
- * @param name      Molecule name.
- * @param amount    Number of molecules to add.
- */
 void addMolecules(plugin::diffusion::Module& diffusion,
     const SizeVector& worldSize, plugin::cell::CellBase& cell,
-    const String& name, unsigned int amount = 1)
+    const String& name, unsigned int amount)
 {
     // Get coordinates
     const auto coords = getCoordinates(diffusion.getGridSize(), worldSize, cell);
@@ -95,18 +91,9 @@ void addMolecules(plugin::diffusion::Module& diffusion,
 
 /* ************************************************************************ */
 
-/**
- * @brief Decrease amount of molecules outside of cell.
- *
- * @param diffusion Diffusion module. Obtained by <code>simulation.useModule<plugin::diffusion::Module>("diffusion");</code>
- * @param worldSize Simulation world size. <code>simulation.getWorldSize();</code>
- * @param cell      Cell object.
- * @param name      Molecule name.
- * @param amount    Number of molecules to add.
- */
 void removeMolecules(plugin::diffusion::Module& diffusion,
     const SizeVector& worldSize, plugin::cell::CellBase& cell,
-    const String& name, unsigned int amount = 1)
+    const String& name, unsigned int amount)
 {
     // Get coordinates
     const auto coords = getCoordinates(diffusion.getGridSize(), worldSize, cell);
@@ -121,14 +108,6 @@ void removeMolecules(plugin::diffusion::Module& diffusion,
 
 /* ************************************************************************ */
 
-/**
- * @brief Receive amount of molecules outside of cell.
- *
- * @param diffusion Diffusion module. Obtained by <code>simulation.useModule<plugin::diffusion::Module>("diffusion");</code>
- * @param worldSize Simulation world size. <code>simulation.getWorldSize();</code>
- * @param cell      Cell object.
- * @param name      Molecule name.
- */
 unsigned int getAmountOfMolecules(plugin::diffusion::Module& diffusion,
     const SizeVector& worldSize, plugin::cell::CellBase& cell,
     const String& name)
@@ -150,6 +129,7 @@ unsigned int getAmountOfMolecules(plugin::diffusion::Module& diffusion,
 
 /* ************************************************************************ */
 
+}
 }
 
 /* ************************************************************************ */
