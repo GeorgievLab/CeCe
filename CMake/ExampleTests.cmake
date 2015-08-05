598cdc742cb16b3612ba194119fc89880bcf5b35
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-08-05 13:50:45 +0200

    Added example testing.

---
+++ CMake/ExampleTests.cmake
@@ -0,0 +1,25 @@
+# ######################################################################### #
+# Georgiev Lab (c) 2015                                                     #
+# ######################################################################### #
+# Department of Cybernetics                                                 #
+# Faculty of Applied Sciences                                               #
+# University of West Bohemia in Pilsen                                      #
+# ######################################################################### #
+
+if (DEV_TEST_EXAMPLES AND ENABLE_CLI)
+    # Get all examples
+    file(GLOB EXAMPLES RELATIVE "${CMAKE_SOURCE_DIR}/examples"
+        "${CMAKE_SOURCE_DIR}/examples/*.(xml|reactions)"
+    )
+
+    # Foreach examples
+    foreach (EXAMPLE ${EXAMPLES})
+        add_test(
+            NAME "test_${EXAMPLE}"
+            COMMAND $<TARGET_FILE:cli> "${CMAKE_SOURCE_DIR}/examples/${EXAMPLE}"
+        )
+
+    endforeach ()
+endif ()
+
+# ######################################################################### #
