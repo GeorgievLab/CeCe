0d0d8e77de6a264e2b0d10933c0dbf9ef8b0207c
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-08-21 16:04:27 +0200

    Implemented shapeToGrid for polygon.

--- simulator/ShapeToGrid.hpp
+++ simulator/ShapeToGrid.hpp
@@ -59,6 +59,10 @@ void mapShapeToGrid(FnIn fnIn, FnOut fnOut, const Shape& shape, const Vector<Ste
     case ShapeType::Rectangle:
         mapShapeToGrid(fnIn, fnOut, shape.getRectangle(), steps, center, rotation, max, min);
         break;
+
+    case ShapeType::Edges:
+        mapShapeToGrid(fnIn, fnOut, shape.getEdges(), steps, center, rotation, max, min);
+        break;
     }
 }
 
@@ -216,6 +220,129 @@ void mapShapeToGrid(FnIn fnIn, FnOut fnOut, const ShapeRectangle& shape, const V
                 fnIn(Vector<T>(coord));
             else
                 fnOut(Vector<T>(coord));
+        }
+    }
+}
+
+/* ************************************************************************ */
+
+/**
+ * @brief Map edges shape to grid.
+ *
+ * @tparam OutIt Output iterator type.
+ * @tparam T     Coordinate value type.
+ * @tparam StepT Step type.
+ *
+ * @param out    Output iterator. It's required to be valid for all incrementations.
+ * @param shape  Edges shape
+ * @param steps
+ * @param center Edges center in grid coordinates.
+ * @param max    Maximum coordinates.
+ * @param min    Minimum coordinates (default is {0, 0}).
+ */
+template<typename OutIt, typename T, typename StepT>
+OutIt mapShapeToGrid(OutIt out, const ShapeEdges& shape, const Vector<StepT>& steps,
+    const Vector<T>& center, units::Angle rotation, const Vector<T>& max,
+    const Vector<T>& min = Zero)
+{
+    mapShapeToGrid(
+        [&out](Vector<T>&& coord) { *out++ = coord; },
+        [](Vector<T>&& coord) {},
+        shape, steps, center, rotation, max, min
+    );
+
+    return out;
+}
+
+/* ************************************************************************ */
+
+/**
+ * @brief Map edges shape to grid.
+ *
+ * @tparam FnIn
+ * @tparam FnOut
+ * @tparam T     Coordinate value type.
+ * @tparam StepT Step type.
+ *
+ * @param fnIn   Callback function to inner coordinates.
+ * @param fnOut  Callback function to outer coordinates.
+ * @param shape  Edges shape
+ * @param steps  Grid cell size.
+ * @param center Edges center in grid coordinates.
+ * @param max    Maximum coordinates.
+ * @param mim    Minimum coordinates (default is {0, 0}).
+ *
+ * @link http://alienryderflex.com/polygon_fill/
+ */
+template<typename FnIn, typename FnOut, typename T, typename StepT>
+void mapShapeToGrid(FnIn fnIn, FnOut fnOut, const ShapeEdges& shape, const Vector<StepT>& steps,
+    const Vector<T>& center, units::Angle rotation, const Vector<T>& max,
+    const Vector<T>& min = Zero)
+{
+    // Get signed type
+    using Ts = typename std::make_signed<T>::type;
+
+    DynamicArray<Ts> nodes;
+    nodes.reserve(10);
+
+    // Alias to edges
+    DynamicArray<Vector<RealType>> edges;
+    edges.reserve(shape.edges.size());
+
+    for (const auto& edge : shape.edges)
+        edges.push_back(center + edge / steps);
+
+    //  Loop through the rows of the image.
+    for (auto y = Ts(min.getY()); y < Ts(max.getY()); ++y)
+    {
+        nodes.clear();
+
+        // Build a list of nodes
+        auto j = edges.size() - 1;
+        for (std::size_t i = 0u; i < edges.size(); ++i)
+        {
+            const auto yi = edges[i].getY();
+            const auto yj = edges[j].getY();
+
+            if ((yi <  static_cast<RealType>(y) &&
+                 yj >= static_cast<RealType>(y)) ||
+                (yj <  static_cast<RealType>(y) &&
+                 yi >= static_cast<RealType>(y)))
+            {
+                nodes.push_back(
+                    edges[i].getX() + (y - yi)
+                    / (yj - yi)
+                    * (edges[j].getX() - edges[i].getX())
+                );
+            }
+            j = i;
+        }
+
+        // Sort the nodes
+        std::sort(nodes.begin(), nodes.end());
+
+        //  Fill the pixels between node pairs.
+        for (std::size_t i = 0u; i < nodes.size(); i += 2)
+        {
+            if (nodes[i] >= static_cast<Ts>(max.getX()))
+            {
+                fnOut(Vector<T>(nodes[i], y));
+            }
+            else if (nodes[i + 1] > static_cast<Ts>(min.getX()))
+            {
+                if (nodes[i] < static_cast<Ts>(min.getX()))
+                    nodes[i] = min.getX();
+
+                if (nodes[i + 1] > static_cast<Ts>(max.getX()))
+                    nodes[i + 1] = max.getX();
+
+                for (Ts x = nodes[i]; x < nodes[i + 1]; ++x)
+                    fnIn(Vector<T>(x, y));
+            }
+            else
+            {
+                fnOut(Vector<T>(nodes[i], y));
+            }
         }
     }
 }
