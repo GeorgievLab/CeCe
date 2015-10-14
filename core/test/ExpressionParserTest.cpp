56255f4885cc8f90e73caf5200d7ab5f2867a26e
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-10-14 15:27:38 +0200

    Removed explicit parameters constructor in test for parseExpression.

--- core/test/ExpressionParserTest.cpp
+++ core/test/ExpressionParserTest.cpp
@@ -35,39 +35,39 @@
 
 TEST(ExpressionParser, basic)
 {
-    EXPECT_FLOAT_EQ(15.f, parseExpression("15", {}));
-    EXPECT_FLOAT_EQ(15.f, parseExpression("15.f", {}));
-    EXPECT_FLOAT_EQ(10.f, parseExpression("2*5", {}));
-    EXPECT_FLOAT_EQ(4.f, parseExpression("2*6/3", {}));
-    EXPECT_FLOAT_EQ(10.f, parseExpression("2 * 5", {}));
-    EXPECT_FLOAT_EQ(9.f, parseExpression("3 * (1 + 2)", {}));
-    EXPECT_FLOAT_EQ(-3.f, parseExpression("3 * (1 - 2)", {}));
-    EXPECT_FLOAT_EQ(13.f, parseExpression("2 * 5 + 3", {}));
-    EXPECT_FLOAT_EQ(11.f, parseExpression("5 + 2 * 3", {}));
-    EXPECT_FLOAT_EQ(constants::PI, parseExpression("pi", {}));
-    EXPECT_FLOAT_EQ(constants::E, parseExpression("e", {}));
+    EXPECT_FLOAT_EQ(15.f, parseExpression("15"));
+    EXPECT_FLOAT_EQ(15.f, parseExpression("15.f"));
+    EXPECT_FLOAT_EQ(10.f, parseExpression("2*5"));
+    EXPECT_FLOAT_EQ(4.f, parseExpression("2*6/3"));
+    EXPECT_FLOAT_EQ(10.f, parseExpression("2 * 5"));
+    EXPECT_FLOAT_EQ(9.f, parseExpression("3 * (1 + 2)"));
+    EXPECT_FLOAT_EQ(-3.f, parseExpression("3 * (1 - 2)"));
+    EXPECT_FLOAT_EQ(13.f, parseExpression("2 * 5 + 3"));
+    EXPECT_FLOAT_EQ(11.f, parseExpression("5 + 2 * 3"));
+    EXPECT_FLOAT_EQ(constants::PI, parseExpression("pi"));
+    EXPECT_FLOAT_EQ(constants::E, parseExpression("e"));
 }
 
 /* ************************************************************************ */
 
 TEST(ExpressionParser, functions)
 {
-    EXPECT_FLOAT_EQ(3.f, parseExpression("sqrt(9)", {}));
-    EXPECT_FLOAT_EQ(10.f, parseExpression("abs(10)", {}));
-    EXPECT_FLOAT_EQ(10.f, parseExpression("abs(-10)", {}));
-    EXPECT_FLOAT_EQ(0.f, parseExpression("log(1)", {}));
-    EXPECT_FLOAT_EQ(0.f, parseExpression("log    (1)", {}));
-    EXPECT_FLOAT_EQ(0.f, parseExpression("sin(0)", {}));
-    EXPECT_FLOAT_EQ(2.f, parseExpression("ln(E^2)", {}));
-    //EXPECT_FLOAT_EQ(0.f, parseExpression("sin(atan(1)*4)", {}));
-    EXPECT_FLOAT_EQ(1.f, parseExpression("cos(2*pi)", {}));
+    EXPECT_FLOAT_EQ(3.f, parseExpression("sqrt(9)"));
+    EXPECT_FLOAT_EQ(10.f, parseExpression("abs(10)"));
+    EXPECT_FLOAT_EQ(10.f, parseExpression("abs(-10)"));
+    EXPECT_FLOAT_EQ(0.f, parseExpression("log(1)"));
+    EXPECT_FLOAT_EQ(0.f, parseExpression("log    (1)"));
+    EXPECT_FLOAT_EQ(0.f, parseExpression("sin(0)"));
+    EXPECT_FLOAT_EQ(2.f, parseExpression("ln(E^2)"));
+    //EXPECT_FLOAT_EQ(0.f, parseExpression("sin(atan(1)*4)"));
+    EXPECT_FLOAT_EQ(1.f, parseExpression("cos(2*pi)"));
 }
 
 /* ************************************************************************ */
 
 TEST(ExpressionParser, variables)
 {
-    EXPECT_FLOAT_EQ(7.f, parseExpression("v1 * v2 + v3", {
+    EXPECT_FLOAT_EQ(7.f, parseExpression("v1 * v2 + v3", Map<String, float>{
         {"v1", 5.f}, {"v2", 2.f}, {"v3", -3.f}
     }));
 }
@@ -80,7 +80,7 @@ TEST(ExpressionParser, inner)
         const char str[] = "15 + 3 hello";
         auto range = makeRange(str);
 
-        EXPECT_FLOAT_EQ(18.f, parseExpressionRef(range, {}));
+        EXPECT_FLOAT_EQ(18.f, parseExpressionRef(range));
         EXPECT_EQ(str + 7, range.begin());
     }
 }
@@ -89,13 +89,13 @@ TEST(ExpressionParser, inner)
 
 TEST(ExpressionParser, invalid)
 {
-    EXPECT_THROW(parseExpression("45-    10 + lol", {}), UnknownConstantException);
+    EXPECT_THROW(parseExpression("45-    10 + lol"), UnknownConstantException);
     EXPECT_THROW(parseExpression("sin()", {}), UnknownConstantException);
-    EXPECT_THROW(parseExpression(" 9 * cos ( 5", {}), MissingParenthesisException);
-    EXPECT_THROW(parseExpression(" 9 * cos ( ", {}), UnknownConstantException);
-    EXPECT_THROW(parseExpression("5 + (10 * (6)", {}), MissingParenthesisException);
-    EXPECT_THROW(parseExpression("\t\b\b\v", {}), EmptyExpressionException);
-    EXPECT_THROW(parseExpression(" ", {}), EmptyExpressionException);
+    EXPECT_THROW(parseExpression(" 9 * cos ( 5"), MissingParenthesisException);
+    EXPECT_THROW(parseExpression(" 9 * cos ( "), UnknownConstantException);
+    EXPECT_THROW(parseExpression("5 + (10 * (6)"), MissingParenthesisException);
+    EXPECT_THROW(parseExpression("\t\b\b\v"), EmptyExpressionException);
+    EXPECT_THROW(parseExpression(" "), EmptyExpressionException);
 }
 
 /* ************************************************************************ */
