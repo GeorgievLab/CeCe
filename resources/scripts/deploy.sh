a0df3bb18e3f0becd8ef5db052a6d694210ef2c1
Jiří Fatka <fatkaj@ntis.zcu.cz>
2015-10-19 10:56:08 +0200

    Added deployed release suffix for non-tag builts.

--- resources/scripts/deploy.sh
+++ resources/scripts/deploy.sh
@@ -5,6 +5,13 @@ if [ ! "$TRAVIS_BRANCH" = "master" -a ! "$TRAVIS_BRANCH" = "travis-ci" ]; then
     exit 0
 fi
 
+SUFFIX=""
+
+# Add suffix for non-tag releases
+if [ -z "$TRAVIS_TAG" ]; then
+    SUFFIX="-$TRAVIS_BRANCH"
+fi
+
 PLATFORM=""
 
 case $TRAVIS_OS_NAME in
@@ -25,6 +32,12 @@ case $TRAVIS_OS_NAME in
 esac
 
 PACKAGE=build/CeCe-$VERSION-$PLATFORM.tar.gz
+
+if [ -n "$SUFFIX" ]; then
+    PACKAGE_NEW=build/CeCe-$VERSION-$PLATFORM-$SUFFIX.tar.gz
+    mv $PACKAGE_SRC $PACKAGE_NEW
+    PACKAGE=$PACKAGE_NEW
+fi
 
 echo "Package: $PACKAGE"
 curl --ftp-create-dirs --ftp-ssl -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_SERVER/bin/ -T $PACKAGE || echo "Deploy failed"
