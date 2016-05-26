52bdfc2b5086377cba4023e929a07af175ab8e1f
Jiří Fatka <fatkaj@ntis.zcu.cz>
2016-05-26 15:30:28 +0200

    Added IterationRange class.

--- cece/simulator/IterationRange.hpp
+++ cece/simulator/IterationRange.hpp
@@ -27,8 +27,8 @@
 
 /* ************************************************************************ */
 
-// C++
-#include <cstddef>
+// CeCe
+#include "cece/simulator/IterationType.hpp"
 
 /* ************************************************************************ */
 
@@ -38,16 +38,96 @@ namespace simulator {
 /* ************************************************************************ */
 
 /**
- * @brief Type for iteration number.
+ * @brief Range of iterations.
  */
-using IterationNumber = std::size_t;
+class IterationRange
+{
 
-/* ************************************************************************ */
+// Public Ctors & Dtors
+public:
 
-/**
- * @brief Type for iteration count.
- */
-using IterationCount = IterationNumber;
+
+    /**
+     * @brief Default constructor.
+     */
+    IterationRange() = default;
+
+
+    /**
+     * @brief Constructor.
+     *
+     * @param iteration Single iteration.
+     */
+    IterationRange(IterationNumber iteration) noexcept
+        : m_first(iteration)
+        , m_last(iteration)
+    {
+        // Nothing to do
+    }
+
+
+    /**
+     * @brief Constructor.
+     *
+     * @param first
+     * @param last
+     */
+    IterationRange(IterationNumber first, IterationNumber last) noexcept
+        : m_first(first)
+        , m_last(last)
+    {
+        // Nothing to do
+    }
+
+
+// Public Accessors
+public:
+
+
+    /**
+     * @brief Returns the first iteration number.
+     *
+     * @return
+     */
+    IterationNumber getFirst() const noexcept
+    {
+        return m_first;
+    }
+
+
+    /**
+     * @brief Returns the last iteration number.
+     *
+     * @return
+     */
+    IterationNumber getLast() const noexcept
+    {
+        return m_last;
+    }
+
+
+    /**
+     * @brief Check if given iteration is in range.
+     *
+     * @param iteration
+     *
+     * @return
+     */
+    bool inRange(IterationNumber iteration) const noexcept
+    {
+        return iteration >= m_first && iteration <= m_last;
+    }
+
+
+// Private Data Members
+private:
+
+    // The first iteration number.
+    IterationNumber m_first{};
+
+    /// The last iteration number.
+    IterationNumber m_last{};
+};
 
 /* ************************************************************************ */
 
