7ac5ba3c6c65c9cf7ac521fd4d911f8863d949f0
Jiří Fatka <fatkaj@ntis.zcu.cz>
2016-06-08 10:48:25 +0200

    Fixed path for static libpng library.

--- scripts/ci/before_build-win32.bat
+++ scripts/ci/before_build-win32.bat
@@ -26,8 +26,15 @@
 @echo on
 
 :: Shared configuration arguments
-:: SET ARGS=-DCMAKE_BUILD_TYPE=release -DDEV_TESTS_BUILD=On -DDEV_PHYSICS_BUILTIN_DEBUG=On -DDEV_PLUGIN_streamlines_RENDER=On -DPNG_PNG_INCLUDE_DIR="C:\Program Files (x86)\libpng\include" -DPNG_LIBRARY_RELEASE="C:\Program Files (x86)\libpng\lib\libpng16.a"
-SET ARGS=-DCMAKE_BUILD_TYPE=Release -DCONFIG_CLI_ENABLE_VIDEO_CAPTURE=Off -DDEV_PHYSICS_BUILTIN_DEBUG=On -DDEV_PLUGIN_streamlines_RENDER=On -DBOOST_ROOT="%BOOST_ROOT%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%" -DPNG_PNG_INCLUDE_DIR="C:\Program Files (x86)\libpng\include" -DPNG_LIBRARY_RELEASE="C:\Program Files (x86)\libpng\lib\libpng16.a"
+SET ARGS=^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCONFIG_CLI_ENABLE_VIDEO_CAPTURE=Off ^
+    -DDEV_PHYSICS_BUILTIN_DEBUG=On ^
+    -DDEV_PLUGIN_streamlines_RENDER=On ^
+    -DBOOST_ROOT="%BOOST_ROOT%" ^
+    -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%" ^
+    -DPNG_PNG_INCLUDE_DIR="C:\Program Files (x86)\libpng\include" ^
+    -DPNG_LIBRARY_RELEASE="C:\Program Files (x86)\libpng\lib\libpng16_static.lib"
 
 md build
 pushd build
